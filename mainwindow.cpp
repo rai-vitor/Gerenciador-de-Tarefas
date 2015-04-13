56cd071fbadd59d0e11a15e3a91ef801d7a4a164
Raí Vitor <raivitor@gmail.com>
2015-04-13 16:36:11 -0300

    Atualizações

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,15 +16,15 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-  model = new QStandardItemModel();
-  QStringList headers;
-  headers << tr("Nome") << tr("Status") << tr("PID") << tr("PPID") << tr("Usuário") << tr("Threads") << tr("Trocas de Contexto");
-  model->setHorizontalHeaderLabels(headers);
+    model = new QStandardItemModel();
+    QStringList headers;
+    headers << tr("Nome") << tr("Status") << tr("PID") << tr("PPID") << tr("Usuário") << tr("Threads") << tr("Trocas de Contexto");
+    model->setHorizontalHeaderLabels(headers);
 
 
-// model->setHorizontalHeaderItem( 0, new QStandardItem( "Nome" ) );
-// model->setHorizontalHeaderItem( 1, new QStandardItem( "Status" ) );
- // model->setHorizontalHeaderItem( 2, new QStandardItem( "PID" ) );
+    // model->setHorizontalHeaderItem( 0, new QStandardItem( "Nome" ) );
+    // model->setHorizontalHeaderItem( 1, new QStandardItem( "Status" ) );
+    // model->setHorizontalHeaderItem( 2, new QStandardItem( "PID" ) );
 
 
     QList<QStandardItem *> listaItem;
@@ -74,7 +74,7 @@ MainWindow::MainWindow(QWidget *parent) :
         hostname.close();
     }
     else {
-        cout << "Não foi possível abrir o arquivo" << endl;
+    //    cout << "Não foi possível abrir o arquivo" << endl;
     }
 
     /* ------------------------------------------------------------------------- */
@@ -92,7 +92,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->label_8->setText("Versão do Kernel: " + qstr);
 
     while ( fgets( buff, 256, Name ) != NULL ) {
-      cout << buff << endl;
+    //  cout << buff << endl;
     }
 
     //Dividindo a string do nome do sistema
@@ -117,7 +117,7 @@ MainWindow::MainWindow(QWidget *parent) :
     FILE *cores = popen("cat /proc/cpuinfo | grep processor | wc -l","r");
 
     while ( fgets( buff, 256, cpu ) != NULL ) {
-      cout << buff << endl;
+     // cout << buff << endl;
     }
 
     //Dividindo a string do nome do sistema
@@ -126,14 +126,27 @@ MainWindow::MainWindow(QWidget *parent) :
     qstr = QString::fromStdString(buff);
     list = qstr.split(":");
     qstr = list[1];
+    list = qstr.split(" ");
+    qstr1 = list[list.size()-1];
 
-   // cout << list[0] << endl;
+    //string t = qstr1;
+
+   // float a = qstr1.toFloat();
+    //cout<<"Aque: "<<t.substr(0, strlen(pch)-3)<<endl;
+    //float a = std::stof (t.substr(0, strlen(t)-3)) ; // Converti pra float
+    cout<<"Aqui: ";
+    qstr1 = qstr1.remove(-4,3);
+
+    float procTotal = qstr1.toFloat();
+    cout<<procTotal;
 
     //Quantidade de Cores
 
     while ( fgets( buff, 256, cores ) != NULL ) {
-      cout << buff << endl;
+     // cout << buff << endl;
     }
+
+
 
     qstr1 = QString::fromStdString(buff);
 
@@ -150,7 +163,7 @@ MainWindow::MainWindow(QWidget *parent) :
      FILE *memswap = popen("cat /proc/meminfo | grep 'MemTotal'","r");
 
      while ( fgets( buff, 256, memram ) != NULL ) {
-       cout << buff << endl;
+       //cout << buff << endl;
      }
 
      qstr = " ";
@@ -165,9 +178,42 @@ MainWindow::MainWindow(QWidget *parent) :
      pclose(memram);
      pclose(memswap);
 
+     /***************************************************************************/
+     while(true){
+
+     }
+     //Processadores
+     FILE *proc = popen("cat /proc/cpuinfo | grep 'MHz'","r");
+
+     qstr = " ";
+     list.clear();
+     while ( fgets( buff, 256, memram ) != NULL ) {
+         list.push_back(buff); // Cada linha tem um processador
+     }
+
+     float proc1 = 0;
+     float proc2 = 0;
+     float proc3 = 0;
+     float proc4 = 0;
+
+     QStringList processadores;
+     for(int i = 0; i<list.size(); i++){
+         qstr = list[i];
+         processadores = qstr.split(" ");
+         qstr = processadores[2];
+         if(i == 0) proc1 = qstr.toFloat();
+         if(i == 1) proc2 = qstr.toFloat();
+         if(i == 2) proc3 = qstr.toFloat();
+         if(i == 3) proc4 = qstr.toFloat();
+     }
 
 
-
+     procTotal = procTotal * 10000.0;
+     cout<<"Proc 1:"<<(proc1*100)/procTotal<<endl;
+     ui->progressBar_1->setValue((proc1*100)/procTotal);
+     ui->progressBar_2->setValue((proc2*100)/procTotal);
+     ui->progressBar_3->setValue((proc3*100)/procTotal);
+     ui->progressBar_4->setValue((proc4*100)/procTotal);
  }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -22,6 +22,7 @@ private:
     Ui::MainWindow *ui;
     QStandardItemModel *model;
     QSplitter *splitter;
+    float procTotal;
 
 };
 
